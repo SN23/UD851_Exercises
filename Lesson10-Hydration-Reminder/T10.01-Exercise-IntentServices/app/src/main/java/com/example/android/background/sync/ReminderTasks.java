c3ebb108ff19993b8adae122d0461f5d796515c7
Sukhjinder <nahal.snahal@gmail.com>
2018-02-18 20:12:07 -0500

    Completed Exercise T10.01

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -1,14 +1,35 @@
 package com.example.android.background.sync;
 
-// TODO (1) Create a class called ReminderTasks
+// Complete (1) Create a class called ReminderTasks
 
-// TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+import android.content.Context;
 
-// TODO (6) Create a public static void method called executeTask
-// TODO (7) Add a Context called context and String parameter called action to the parameter list
+import com.example.android.background.utilities.PreferenceUtilities;
 
-// TODO (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+public class ReminderTasks {
 
-// TODO (3) Create a private static void method called incrementWaterCount
-// TODO (4) Add a Context called context to the argument list
-// TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+    // Complete (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+    public static String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+
+    // Complete (3) Create a private static void method called incrementWaterCount
+    // Complete (4) Add a Context called context to the argument list
+    private static void incrementWaterCount(Context context) {
+
+        // Complete (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+        PreferenceUtilities.incrementWaterCount(context);
+    }
+
+    // Complete (6) Create a public static void method called executeTask
+    // Complete (7) Add a Context called context and String parameter called action to the parameter list
+
+    public static void executeTask(Context context, String action) {
+        // Complete (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+        if (action.equals(ReminderTasks.ACTION_INCREMENT_WATER_COUNT)) {
+            incrementWaterCount(context);
+        }
+    }
+
+}
+
+
+
